2fc57b5d76d670461f33ec355c833ef96d2adb79
Hajar Alhajeh <115671004+Hajar013@users.noreply.github.com>
2024-07-22 20:56:52 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,3 @@
-public class Logger {
-   
-    private static Logger instance;
-
-   
-    private Logger() {}
-
-   
-    public static synchronized Logger getInstance() {
-        if (instance == null) {
-            instance = new Logger();
-        }
-        return instance;
-    }
-
-   
-    public void log(String message) {
-        System.out.println("Log: " + message);
-    }
-}
-
-
 public class Main {
     public static void main(String[] args) {
       
